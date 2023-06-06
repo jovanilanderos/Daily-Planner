b4b34c04430f296cb372f8a0e619f50031ba4066
jovani <jovanilanderos@gmail.com>
2023-06-05 23:01:00 -0500

    this function generates time blocks for hours of the day. each block consists of an hour diplay, and a discription text area

--- script.js
+++ script.js
@@ -31,5 +31,23 @@ $(function () {
         var currentDate = dayjs().format("dddd, MMMM D, YYYY");
         $("#currentDay").text(currentDate);
       }
-    
+
+  function generateTimeBlocks() {
+    var container = $(".container-lg");
+    for (var hour = 0; hour < 24; hour++) {
+      var timeBlock = $('<div class="row time-block"></div>');
+      var hourText = $('<div class="col-2 col-md-1 hour text-center py-3"></div>');
+      var description = $('<textarea class="col-8 col-md-10 description" rows="3"></textarea>');
+      var saveButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button>');
+
+      hourText.text(formatHour(hour));
+      description.attr("id", "hour-" + hour);
+      saveButton.attr("id", "saveBtn-" + hour);
+
+      timeBlock.append(hourText, description, saveButton);
+      container.append(timeBlock);
+    }
+  }
+
+
   });
