087f7added2db0c785bd7ff61f2459c5b078de72
jovani <jovanilanderos@gmail.com>
2023-06-05 22:30:55 -0500

    This function ensures that the styling of the time blocks is updated dynamically based on the hour or hour changes.

--- script.js
+++ script.js
@@ -5,5 +5,19 @@ $(function () {
       localStorage.setItem(timeBlockId, description);
     });
   
-   
+  function applyTimeBlockClasses() {
+    var currentHour = dayjs().format("H");
+    $(".time-block").each(function () {
+      var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+      $(this).removeClass("past present future");
+      if (timeBlockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (timeBlockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
   });
